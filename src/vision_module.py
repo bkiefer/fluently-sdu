b0fa48e05caba2d19e354bae5fdc40164f430652
Guglielmo Borzone <borzoneg@gmail.com>
2025-07-23 14:06:58 +0200

    comments on vision module

--- src/vision_module.py
+++ src/vision_module.py
@@ -16,7 +16,16 @@ import os
 
 class VisionModule():
     def __init__(self, camera_Ext: sm.SE3, verbose=False):
-        # camera initialization
+        """initialization of the vision module
+        In case the camera is not connected the try except block arn the user and set the camera to None, 
+        the functions in this module have try except in case they need to use the camera so that you can
+        work without using the actual camera.
+        Then load all the models used for classification and localization
+
+        Args:
+            camera_Ext (sm.SE3): Extrinsic of the camera
+            verbose (bool, optional): Set this to true to see all the debug messages in the console. Defaults to False.
+        """
         console_level = 'debug' if verbose else 'info'
         self.logger = utilities.CustomLogger("Vision", "MeMVision.log", console_level=console_level, file_level=None)
         try:
@@ -36,14 +45,22 @@ class VisionModule():
         self.set_background()
         
     def set_background(self):
+        """Save the current frame as background, mainly used in the verification of the pickup, when the cells are confirmed we save the background and then compare as we pick up
+        """
         self.logger.debug("Setting background")
         new_bg = self.get_current_frame()
         self.background = new_bg
 
-    def get_current_frame(self, format="cv2", wait_delay=0) -> np.ndarray:
-        """get the current frame from the camera
+    def get_current_frame(self, format="cv2", wait_delay=0)-> np.ndarray:
+        """get the current frame from the camera, in case the camera has not been connected a file from storage gets used
+        the frame is retrieved as cv2 image but can be converted in pil, this is the format used by the gui canvas
+
+        Args:
+            format (str, optional): format of the frame. Defaults to "cv2".
+            wait_delay (int, optional): delay before taking the image, could be sued to have a more static and higher quality frame. Defaults to 0.
+
         Returns:
-            np.ndarray: frame
+            np.ndarray: frame from camera/storage
         """
         time.sleep(wait_delay)
         try :
@@ -51,13 +68,21 @@ class VisionModule():
         except AttributeError:
             # frame = cv2.imread("data/camera_frame.png")
             frame = cv2.imread("data/camera_frame1.png")
-            # frame = cv2.imread("data/camera_frame1.jpg")
         if format.lower() == "pil":
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
             frame = PIL.Image.fromarray(frame)
         return frame
 
-    def get_z_at_pos(self, x, y):
+    def get_z_at_pos(self, x, y)-> float:
+        """Get the z from the depth sensor of the camera in the position specified
+
+        Args:
+            x (int): x position of the point
+            y (int): y position of the point
+
+        Returns:
+            float: z distance from camera to point xy in camera
+        """
         depth_frame = self.camera.get_depth_frame()
         depth_img = np.asanyarray(depth_frame.get_data())
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_img, alpha=100), cv2.COLORMAP_JET)
@@ -65,8 +90,15 @@ class VisionModule():
         z = depth_frame.get_distance(x, y)
         return z
 
-    def locate_pack(self, frame: ndarray):
-        # result = self.packs_yolo_model(frame)
+    def locate_pack(self, frame: ndarray)-> dict: 
+        """run the yolo image on the frame and return a dictionary describing the result for the pack
+
+        Args:
+            frame (ndarray): image to use for detection/classification
+
+        Returns:
+            dict: dictionary with shape(label), size, if the cover is on(it is always true as the classification is don ON the cover), and the location in the frame
+        """
         result = self.packs_yolo_model.predict(frame, verbose=False)
         if len(result[0].boxes) == 0:
             return None
@@ -78,17 +110,17 @@ class VisionModule():
         # result[0].show()
         return {'shape': label, 'size': (int(xywh[2]), int(xywh[3])), 'cover_on': True, 'location': (int(xywh[0]), int(xywh[1]))}
 
-    def identify_cells(self, frame: ndarray, drawing_frame:ndarray=None) -> dict[tuple[str, float]]:
+    def identify_cells(self, frame: ndarray, drawing_frame: ndarray=None)-> dict:
         """
-        classify the cell model from one or multiple frames
+        classify the cell model from one frame using the yolo model
 
         Args:
-            frame (list[np.ndarray]): list of frame(s) used for the classification 
+            frame np.ndarray: frame used for the classification 
+            drawing_frame np.ndarray: if provided the function will draw on this frame the results of the classification
 
         Returns:
-            list[tuple[str, float]]: list of model with associated probability
+            dict[bbs: list[list[int]], zs: list[float], model: str]: a dctionary with all the bb(bounding boxes, zs associated and the model for all the cell)
         """
-        #cells_probs = [{'model': "AA", 'prob': 0.51}, {'model': "C", 'prob': 0.49},  {'model': "XXX", 'prob': 0.23}, {'model': "XYZ", 'prob': 0.12}]
         result = self.cells_yolo_model.predict(frame, verbose=False) 
         if len(result[0].boxes) == 0:
             return None
@@ -120,17 +152,16 @@ class VisionModule():
         output['model'] = (max(set(models), key=models.count)) 
         return output
 
-    def assess_cells_qualities(self, frame:np.ndarray, bbs_positions: list[ndarray]) -> list[float]:
-        """assign to each cell a score based on quality
+    def assess_cells_qualities(self, frame:np.ndarray, bbs_positions: list[ndarray])-> list[float]:
+        """for each cell tell if is is to keep or not based on a close up and the use of the model
 
         Args:
             frame (np.ndarray): frame for assessment
             bbs_positions (list[ndarray]): the position of the bounding boxes to create a close up of the battery cell
 
         Returns:
-            list[float]: the scores evaluated by the system
+            list[float]: whether or not to keep the cells 
         """
-        # cells_keeps = np.random.choice([True, False], len(bbs_positions))
         cells_keep = []
         img = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
         for bb in bbs_positions:
@@ -139,17 +170,18 @@ class VisionModule():
             cells_keep.append(self.qual_cnn.predict_img(close_up))
         return cells_keep
 
-    def frame_pos_to_pose(self, frame_pos:ndarray, base_T_TCP, Z=None) -> sm.SE3:
+    def frame_pos_to_pose(self, frame_pos:ndarray, base_T_TCP:sm.SE3, Z=None)-> sm.SE3:
         """convert a position in the frame into a 4x4 pose in world frame
 
         Args:
             frame_pos (ndarray): position in the frame
+            base_T_TCP (sm.SE3): T for base to TCP 
+            Z (_type_, optional): distance from camera to pos, can be provided by user or retrieved from depth sensor in camera. Defaults to None.
 
         Returns:
-            sm.SE3(sm.SE3): 4x4 pose in world frame
+            sm.SE3: real world pose of the frame point 
         """
         try:
-            
             Z = self.get_z_at_pos(*frame_pos) if Z is None else Z
             P = vision.frame_pos_to_3dpos(frame_pos=frame_pos, camera=self.camera, Z=Z)
             base_T_cam = base_T_TCP * self.camera.extrinsic
@@ -160,14 +192,23 @@ class VisionModule():
             T = sm.SE3([-1, -1, -1])
         return T
 
-    def verify_pickup(self, position: ndarray, radius=0.5) -> list[bool]:
-        """verify if a cell hs been picked up
+    def verify_pickup(self, position: ndarray, radius=0.5)-> bool:
+        """verify if a cell has been picked up:
+        - we take the background and the current frame
+        - find the absolute difference between the two
+        - threshold the differnce to have white pixel only where there is a difference
+        - dilate the thresholded frame to fill in possible missed pixel by the threshold
+        - at this point we have a black frame, and white pixels where there was a change from the background, these spots are where cell are pickedup
+        - so we start in the position and look in a radius, every white pixel gets added to votes
+        - if 51% of the pixel in the circle are white it gets considered pickedup as there are enough diff in the area
+        - from the background
 
         Args:
-            position (ndarray): position of the cell in the image
+            position (ndarray): position in the frame
+            radius (float, optional): radius around which we check for pixels after the subtraction. Defaults to 0.5.
 
         Returns:
-            bool: if or not the cell was picked up
+            bool: whether or not the cell was pickedup 
         """
         cp_current_frame = copy.deepcopy(self.get_current_frame())
         cp_start_frame = copy.deepcopy(self.background)
